ac762e9b459109ad30ecaffb72f1d1f72e457e79
clement893 <clement@nukleo.com>
2025-12-28 18:02:03 -0500

    fix: Correct 022_add_user_permissions to depend on 021_add_notifications

--- backend/alembic/versions/022_add_user_permissions_table.py
+++ backend/alembic/versions/022_add_user_permissions_table.py
@@ -14,7 +14,7 @@ from sqlalchemy import inspect
 
 # revision identifiers, used by Alembic.
 revision: str = '022_add_user_permissions'
-down_revision: Union[str, None] = '023_merge_migration_heads'
+down_revision: Union[str, None] = '021_add_notifications'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
